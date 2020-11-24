548f6e200d115f13919388051c1e4ad0ed6a0983
Paulo Vitor <paulopnxt@gmail.com>
2020-11-24 15:44:29 -0300

    Back-end

---
+++ back-end/src/app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import Youch from 'youch';
+import path from 'path';
+
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use(cors());
+    this.server.use(
+      '/tf',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'upload'))
+    );
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal Server Error' });
+    });
+  }
+}
+export default new App().server;
---
+++ back-end/src/routes.js
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+
+const routes = new Router();
+
+routes.post('/tf', async (req, res) => {
+    
+});
+
+export default routes;
